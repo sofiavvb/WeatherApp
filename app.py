390626b1a26edc87d5f6f421b86e5151df7ca6e7
sofiavvb <s252974@dac.unicamp.br>
2024-07-13 11:50:45 -0300

    css menu done && token issued fix

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask, request, render_template, redirect, url_for
-import weather_info
+from flask import Flask, request, render_template, redirect, url_for, session
+from weather_info import get_weather_info
 
 app = Flask(__name__)
+app.secret_key = "teste"
 
-@app.route('/menu', '/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def menu():
     #Digitar nome da cidade
+    if request.method == "POST":
+        city = request.form["city"]
+        session["city"] = city
+        return redirect(url_for("result"))
     #clicar botao: chamar função get_weather_info e redireciona para a página de resultados
     return render_template("menu.html")
 
 @app.route('/result', methods=['GET', 'POST'])
 def result():
-    return render_template("result.html")
-
+    try:
+        city, weather_cond, temp, humidity, date = get_weather_info(session["city"])
+        return render_template("result.html", city=city, weather_cond=weather_cond, temp=temp, humidity=humidity, date=date)
+    except:
+        return get_weather_info(session["city"])
+    
 if __name__ == '__main__':
     app.run(debug=True)
--- weather_info.py
+++ weather_info.py
@@ -6,24 +6,29 @@ from functions import saveDataToJson
 #Para usar a api do open weather
 API_KEY = os.getenv("API_KEY")
 
-def get_weather_info():
-    city = input("Choose a city: ")
+def get_weather_info(city) -> tuple[str, str, float, float, str]:
     lang = "pt_br"
     date = datetime.now()
     date = date.strftime('%Y-%m-%d %H:%M:%S')
 
     link = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang={lang}&units=metric"
+    try:
+        #pegar informações da API (get)
+        request = requests.get(link)
+        #converter para dicionario
+        request_dict = request.json()
 
-    #pegar informações da API (get)
-    request = requests.get(link)
-    #converter para dicionario
-    request_dict = request.json()
-
-    weather_cond = request_dict['weather'][0]['description']
-    temp = request_dict['main']['temp']
-    humidity = request_dict['main']['humidity']
-    #append in the sheets
-    update_sheet([[city, weather_cond, temp, humidity, date]])
-    #transform to json object
-    saveDataToJson(city, weather_cond, temp, humidity, date)
-    print("--Info added to the sheet!--")
+        weather_cond = request_dict['weather'][0]['description']
+        temp = request_dict['main']['temp']
+        humidity = request_dict['main']['humidity']
+        #append in the sheets
+        update_sheet([[city, weather_cond, temp, humidity, date]])
+        #transform to json object
+        saveDataToJson(city, weather_cond, temp, humidity, date)
+        
+        return(city, weather_cond, temp, humidity, date)
+    
+    except requests.exceptions.RequestException:
+        return "Erro na conexão com a API", "", 0.0, 0.0, ""
+    except KeyError:
+        return "Resposta inválida da API", "", 0.0, 0.0, ""
